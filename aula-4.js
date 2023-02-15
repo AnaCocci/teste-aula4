30f9828363339604e69c4c76503d1e45072c7fe5
Ana Carolina Silva Cocci <123197150+AnaCocci@users.noreply.github.com>
2023-02-15 19:11:03 -0300

    Aula 4

---
+++ aula-4.js
@@ -0,0 +1,8 @@
+const fruta1 = "banana";
+const fruta2 = "maçã";
+const fruta3 = "laranja";
+
+console.log('Essa é minha lista de frutas:');
+console.log(fruta1);
+console.log(fruta2);
+console.log(fruta3);
